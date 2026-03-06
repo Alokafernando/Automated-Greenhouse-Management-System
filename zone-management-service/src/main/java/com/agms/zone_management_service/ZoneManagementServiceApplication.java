838ba4f12683d44df6c33cdfd4d14ee3bd59e291
Alokafernando <buddhikaaloka2005@gmail.com>
2026-03-07 00:09:13 +0530

    initial zone-management-service

---
+++ zone-management-service/src/main/java/com/agms/zone_management_service/ZoneManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.zone_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneManagementServiceApplication.class, args);
+	}
+
+}
