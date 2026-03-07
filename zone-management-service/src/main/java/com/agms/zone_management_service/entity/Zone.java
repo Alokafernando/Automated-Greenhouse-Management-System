25e6e0a561a0a94f8f65291249dd2fd15b31c12e
Alokafernando <buddhikaaloka2005@gmail.com>
2026-03-07 19:23:50 +0530

    implement zone entity

---
+++ zone-management-service/src/main/java/com/agms/zone_management_service/entity/Zone.java
@@ -0,0 +1,33 @@
+package com.agms.zone_management_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Zone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double minTemp;
+    private double maxTemp;
+
+    private double minHumidity;
+    private double maxHumidity;
+
+    private String deviceId;
+
+}
